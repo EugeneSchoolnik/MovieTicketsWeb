894ace285cc7c8b9505ded913f615fec76ea323a
Eugene <kn23-ye.piskalenko@nubip.edu.ua>
2024-02-09 14:09:50 +0200

    Password Restore page layout

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./pages/Home/Home.vue";
 import Auth from "./pages/Auth/Auth.vue";
+import RestorePass from "./pages/RestorePass/RestorePass.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -8,6 +9,7 @@ const router = createRouter({
     { path: "/", component: Home },
     { path: "/register", component: Auth },
     { path: "/login", component: Auth },
+    { path: "/restorepass", component: RestorePass },
   ],
 });
 

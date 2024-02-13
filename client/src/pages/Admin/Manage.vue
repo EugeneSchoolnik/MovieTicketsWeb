b70b41045c3844407f0e96dce7fbf3edd384fc04
Eugene <kn23-ye.piskalenko@nubip.edu.ua>
2024-02-13 17:19:21 +0200

    Manage page layout

--- client/src/pages/Admin/Manage.vue
+++ client/src/pages/Admin/Manage.vue
@@ -14,7 +14,24 @@ export default {
 </script>
 <template>
   <div :class="s.manage">
-    <ul></ul>
+    <RouterLink to="/admin/new" :class="`${s.add} btn`">Add movie</RouterLink>
+    <div :class="s.list">
+      <MovieItem
+        :movie="{
+          id: '0140149124',
+          title: 'Avangers 3',
+          duration: '120',
+          genres: ['fantasy', 'action'],
+          time: '02:00pm',
+          price: '4',
+          banner:
+            'https://c4.wallpaperflare.com/wallpaper/361/823/829/avengers-endgame-iron-man-robert-downey-jr-captain-america-chris-evans-hd-wallpaper-preview.jpg',
+        }"
+        :admin="true"
+        v-for="(_, i) of Array.from({ length: 8 })"
+        :key="i"
+      />
+    </div>
   </div>
 </template>
 <style src="./manage.module.scss" lang="scss"></style>

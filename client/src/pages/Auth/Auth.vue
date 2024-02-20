0854e05126d7f3c2e1838bc0fa5775cb71ad040c
EugeneSchoolnik <kn23-ye.piskalenko@nubip.edu.ua>
2024-02-20 17:36:48 +0200

    Resolve merge conflict

---
+++ client/src/pages/Auth/Auth.vue
@@ -0,0 +1,92 @@
+<script lang="ts">
+import s from "./auth.module.scss";
+import Input from "../../components/Input/Input.vue";
+import { errors } from "../Admin/MovieForm/checkForm";
+import server from "../../utils/axiosInstance";
+
+export default {
+  name: "auth",
+  components: { Input },
+  data() {
+    return {
+      isLogin: true,
+      user: {
+        email: {
+          value: "",
+          error: "",
+        },
+        password: {
+          value: "",
+          error: "",
+        },
+      },
+      serverError: "",
+      s,
+    };
+  },
+  methods: {
+    getAction() {
+      this.isLogin = this.$route.path === "/login";
+    },
+    onChangeAction() {
+      setTimeout(this.getAction);
+    },
+    onSumbit() {
+      const [email, password] = [this.user.email, this.user.password];
+      (email.error = ""), (password.error = "");
+      if (!email.value) return (this.user.email.error = errors.empty);
+      if (!password.value) return (this.user.password.error = errors.empty);
+
+      server
+        .post(`/auth/${this.isLogin ? "login" : "register"}`, {
+          email: email.value,
+          password: password.value,
+        })
+        .then(({ data }) => {
+          this.serverError = "";
+
+          this.$store.commit("user.setUser", data.data);
+          this.$router.push("/profile");
+        })
+        .catch(e => {
+          if (e.response) this.serverError = e.response.data.message;
+          else console.log(e);
+        });
+    },
+  },
+  mounted() {
+    const redirect = () => {
+      if (this.$store.state.user.loaded) {
+        if (this.$store.state.user.id) this.$router.push("/profile");
+      } else setTimeout(redirect, 100);
+    };
+    redirect();
+    this.getAction();
+    const email = (this.$route.query.email || "") as string;
+    email && (this.user.email.value = email);
+  },
+};
+</script>
+<template>
+  <div :class="s.auth">
+    <RouterLink to="/" :class="s.goHome">
+      <img src="../../assets/img/logo.jpg" alt="logo" />
+      <h1>FilmFlicks</h1>
+    </RouterLink>
+    <h1>{{ isLogin ? "Authorization" : "Registration" }}</h1>
+    <form @submit.prevent="onSumbit">
+      <Input label="Email" v-model="user.email.value" :value="user.email.value" :error="user.email.error" />
+      <Input label="Password" v-model="user.password.value" :error="user.password.error" />
+      <span :class="s.serverError">{{ serverError }}</span>
+      <button class="btn">{{ isLogin ? "Log in" : "Register" }}</button>
+      <p v-if="isLogin">
+        You don't have an account yet? <RouterLink @click="onChangeAction()" to="/register">Sign up</RouterLink>
+      </p>
+      <p v-else>Already have an account? <RouterLink @click="onChangeAction()" to="/login">Sign in</RouterLink></p>
+      <p v-if="isLogin">
+        Forgot Password? <RouterLink @click="onChangeAction()" to="/restorepass">Restore</RouterLink>
+      </p>
+    </form>
+  </div>
+</template>
+<style src="./auth.module.scss" lang="scss"></style>

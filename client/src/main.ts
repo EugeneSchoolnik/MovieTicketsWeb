c25b913e99e6e87ad3cc49553d8adbd659c74cb4
EugeneSchoolnik <kn23-ye.piskalenko@nubip.edu.ua>
2024-02-20 14:44:00 +0200

    Troubleshooting Authorization page

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from "vue";
 import VueSplide from "@splidejs/vue-splide";
 import router from "./router";
-import store from "./store";
 import "./assets/styles/style.scss";
 import App from "./App.vue";
 
 const app = createApp(App);
 app.use(VueSplide);
 app.use(router);
-app.use(store);
 app.mount("#app");
